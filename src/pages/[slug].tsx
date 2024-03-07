0bafba2d81c26a0db253ad2f9c239ff8c7e5f402
Jan Adam Halama <adamhalama@outlook.com>
2024-03-07 10:03:54 +0100

    Profile page with ssg helper

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { api } from "~/utils/api";
+import Image from "next/image";
 
 export const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({
@@ -12,9 +13,20 @@ export const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
       <Head>
         <title>{`@${data.username}`}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>{`@${data.username}`}</div>
-      </main>
+      <PageLayout>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profilePicture}
+            alt={`${data.username}'s profile picture`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${data.username ?? ""}`}</div>
+        <div className="w-full border-b border-slate-400"></div>
+      </PageLayout>
     </>
   );
 };
@@ -24,6 +36,7 @@ import type { GetStaticProps, NextPage } from "next";
 import superjson from "superjson";
 import { appRouter } from "~/server/api/root";
 import { db } from "~/server/db";
+import { PageLayout } from "~/components/layout";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const helpers = createServerSideHelpers({
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import { type RouterOutputs, api } from "~/utils/api";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import Link from "next/link";
+import { PageLayout } from "~/components/layout";
 
 dayjs.extend(relativeTime);
 
@@ -129,20 +130,17 @@ export default function Home() {
 
   return (
     <>
-      <main className="flex h-screen justify-center">
-        <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
-          <div className="border-b border-slate-400 p-4">
-            {isSignedIn && (
-              <div className="flex justify-center">
-                <SignInButton />
-              </div>
-            )}
-            {isSignedIn && <CreatePostWizard />}
-          </div>
-
-          <Feed />
+      <PageLayout>
+        <div className="border-b border-slate-400 p-4">
+          {isSignedIn && (
+            <div className="flex justify-center">
+              <SignInButton />
+            </div>
+          )}
+          {isSignedIn && <CreatePostWizard />}
         </div>
-      </main>
+        <Feed />
+      </PageLayout>
     </>
   );
 }
