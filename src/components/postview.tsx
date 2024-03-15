3745317cf7df92aa77dea662c2f7263b5dc610ad
Jan Adam Halama <adamhalama@outlook.com>
2024-03-15 18:29:04 +0100

    Visual UX improvements, hover:

--- src/components/postview.tsx
+++ src/components/postview.tsx
@@ -14,20 +14,23 @@ export const PostView = (props: PostWithUser) => {
 
   return (
     <Link href={`/post/${post.id}`}>
-      <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
+      <div
+        key={post.id}
+        className="flex gap-3 border-b border-slate-400 p-4 transition-colors duration-200 hover:bg-gray-900"
+      >
         <Link href={`/@${author.username}`}>
-            <Image
-              src={author.profilePicture}
-              className="size-14 rounded-full"
-              alt={`@${author.username}'s profile picture`}
-              width={56}
-              height={56}
-            />
+          <Image
+            src={author.profilePicture}
+            className="size-14 rounded-full transition-opacity duration-200 hover:opacity-75"
+            alt={`@${author.username}'s profile picture`}
+            width={56}
+            height={56}
+          />
         </Link>
 
         <div className="flex flex-col">
           <div className="flex gap-1 text-slate-300">
-            <Link href={`/@${author.username}`}>
+            <Link href={`/@${author.username}`} className="hover:underline">
               <span>{`@${author.username}`}</span>
             </Link>
             <span className="font-thin">
