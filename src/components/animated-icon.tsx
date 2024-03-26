df0cfeeaa3dc6d6d7da286fbb034e14ac9af6342
Jan Adam Halama <adamhalama@outlook.com>
2024-03-26 19:07:39 +0100

    use TW classes for everything, comment count number select-none

--- src/components/animated-icon.tsx
+++ src/components/animated-icon.tsx
@@ -17,16 +17,16 @@ const AnimatedIcon: React.FC<AnimatedIconProps> = ({
 }) => (
   <div className="group flex">
     <div className="relative inline-block">
-      <div className="custom-transition custom-halo-effect bg-custom-blue bg-blend-screen shadow-custom"></div>
+      <div className="custom-transition-200 custom-halo-effect bg-custom-blue bg-blend-screen shadow-custom"></div>
       <Image
         src={src}
         alt={alt}
         width={width}
         height={height}
-        className="custom-transition relative z-10 group-hover:mix-blend-overlay"
+        className="custom-transition-200 relative z-10 group-hover:mix-blend-overlay"
       />
     </div>
-    <span className="custom-transition pl-1 text-sm text-slate-300 group-hover:text-blue-500">
+    <span className="custom-transition-200 select-none pl-1 text-sm text-slate-300 group-hover:text-blue-500">
       {count}
     </span>
   </div>
