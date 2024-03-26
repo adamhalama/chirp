796d7884494adb297abcd495e0b4e6ba422c1d95
Jan Adam Halama <adamhalama@outlook.com>
2024-03-26 19:10:32 +0100

    lint

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -42,7 +42,7 @@ export const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
             alt={`${data.username}'s profile picture`}
             width={128}
             height={128}
-            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
+            className="absolute bottom-0 left-0 mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
           />
         </div>
         <div className="h-[64px]"></div>
